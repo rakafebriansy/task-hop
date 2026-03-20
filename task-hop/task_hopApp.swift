34390693aed16afbd46d2fabaf8a1ec9a77a0c47
rakafebriansy <rakafebrian19@gmail.com>
2026-03-20 11:14:09 +0700

    Initial Commit

---
+++ task-hop/task_hopApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_hopApp.swift
+//  task-hop
+//
+//  Created by Raka Febrian Syahputra on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct task_hopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
